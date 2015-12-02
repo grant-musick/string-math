4ea8c035248d2bd1de8bf94bb1b98ef8423670b6
grant-musick <grant@cozi.com>
2015-12-01 19:31:19 -0800

    Restructrued and updated tests to expected values. Now just need to make the code work against them.

--- python/string_subtraction_tests.py
+++ python/string_subtraction_tests.py
@@ -8,111 +8,105 @@ from string_subtraction import StringNumber
 
 
 
-
+# There are other ways to do this but thought it would be fun working out
+# the problems of a class with dynamically created methods
 class StringNumberEqualityTests(unittest.TestCase):
-    def test_something(self):
-        pass
+    """Shell class for dyanmically generated tests"""
+    pass
+
+    @classmethod
+    def initialize_equality_tests(cls):
+        zero = StringNumber("0")
+        pos1 = StringNumber("1230")
+        pos2 = StringNumber("456")
+        neg1 = StringNumber("-345")
+        neg2 = StringNumber("-11234")
 
 
+        test_items = [
+                    (zero, zero, operator.eq, True),
+                    (zero, zero, operator.lt, False),
+                    (zero, zero, operator.le, True),
+                    (zero, zero, operator.gt, False),
+                    (zero, zero, operator.ge, True),
+                    (zero, zero, operator.ne, False),
 
-def initialize_equality_tests():
-    zero = StringNumber("0")
-    pos1 = StringNumber("1230")
-    pos2 = StringNumber("456")
-    neg1 = StringNumber("-111")
-    neg2 = StringNumber("-333")
+                    (pos1, pos2, operator.eq, False),
+                    (pos1, pos2, operator.lt, False),
+                    (pos1, pos2, operator.le, False),
+                    (pos1, pos2, operator.gt, True),
+                    (pos1, pos2, operator.ge, True),
+                    (pos1, pos2, operator.ne, True),
+                    
+                    (pos2, pos1, operator.eq, False),
+                    (pos2, pos1, operator.lt, True),
+                    (pos2, pos1, operator.le, True),
+                    (pos2, pos1, operator.gt, False),
+                    (pos2, pos1, operator.ge, False),
+                    (pos2, pos1, operator.ne, True),
+
+                    (pos1, neg1, operator.eq, False),
+                    (pos1, neg1, operator.lt, False),
+                    (pos1, neg1, operator.le, False),
+                    (pos1, neg1, operator.gt, True),
+                    (pos1, neg1, operator.ge, True),
+                    (pos1, neg1, operator.ne, True),
+
+                    (neg1, pos1, operator.eq, False),
+                    (neg1, pos1, operator.lt, True),
+                    (neg1, pos1, operator.le, True),
+                    (neg1, pos1, operator.gt, False),
+                    (neg1, pos1, operator.ge, False),
+                    (neg1, pos1, operator.ne, True),
+
+                    (neg1, neg2, operator.eq, False),
+                    (neg1, neg2, operator.lt, False),
+                    (neg1, neg2, operator.le, False),
+                    (neg1, neg2, operator.gt, True),
+                    (neg1, neg2, operator.ge, True),
+                    (neg1, neg2, operator.ne, True),
+
+                    (neg2, neg1, operator.eq, False),
+                    (neg2, neg1, operator.lt, True),
+                    (neg2, neg1, operator.le, True),
+                    (neg2, neg1, operator.gt, False),
+                    (neg2, neg1, operator.ge, False),
+                    (neg2, neg1, operator.ne, True),
+
+                    ]
 
 
-    test_items = [
-                (zero, zero, operator.eq, True),
-                (zero, zero, operator.lt, False),
-                (zero, zero, operator.le, True),
-                (zero, zero, operator.gt, False),
-                (zero, zero, operator.ge, True),
-                (zero, zero, operator.ne, True)
-                ]
+        def my_generator(x, y, op, answer):
+                def test(self):
+                    self.assertEqual(op(x,y), answer, 
+                        "{0} {1} {2} not {3}".format(x, op, y, answer))
+                return test
 
 
-    def my_generator(x, y, op, answer):
-            def test(self):
-                self.assertEqual(op(x,y), answer, 
-                    "{0} {1} {2} not {3}".format(x, op, y, answer))
-            return test
+        operator_nice_names = {
+            operator.eq : "equals",
+            operator.lt : "less than",
+            operator.le : "less than or equal",
+            operator.gt : "greater than",
+            operator.ge : "greater than or equal",
+            operator.ne : "not equal to"
+        }
+
+        for x, y, op, answer in test_items:
+            test_name = "test_{0}_{2}_{1}_{3}".format(x, y,
+                operator_nice_names[op].replace(" ", "_"),
+                answer
+                )
+            test = my_generator(x, y, op, answer)
+            setattr(StringNumberEqualityTests, test_name, test)
 
 
-    operator_nice_names = {
-        operator.eq : "equals",
-        operator.lt : "less than",
-        operator.le : "less than or equal",
-        operator.gt : "greater than",
-        operator.ge : "greater than or equal",
-        operator.ne : "not equal to"
-    }
-
-    for x, y, op, answer in test_items:
-        test_name = "test_{0}_{2}_{1}_{3}".format(x, y,
-            operator_nice_names[op].replace(" ", "_"),
-            answer
-            )
-        test = my_generator(x, y, op, answer)
-        setattr(StringNumberEqualityTests, test_name, test)
-
-
-# Dynamically assign test methods
-initialize_equality_tests()
+# Dynamically assign test methods to class on import
+StringNumberEqualityTests.initialize_equality_tests()
 
 
 
 class StringNumberTests(unittest.TestCase):
-
-
-    def test_comparisons(self):
-        # composite test. technically unittests should test one thing,
-        # but whatever.
-        zero = StringNumber("0")
-        pos1 = StringNumber("1230")
-        pos2 = StringNumber("456")
-        neg1 = StringNumber("-111")
-        neg2 = StringNumber("-333")
-
-        test_items = [
-            (zero, zero, operator.eq, True),
-            (zero, zero, operator.lt, False),
-            (zero, zero, operator.le, True),
-            (zero, zero, operator.gt, False),
-            (zero, zero, operator.ge, True),
-            (zero, zero, operator.ne, True)
-        ]
-
-        for x, y, op, answer in test_items:
-            self.assertEqual(op(x,y), answer, 
-                "{0} {1} {2} not {3}".format(x, op, y, answer))
-
-
-    def test_lt_same_numbers_pos(self):
-        pass
-
-    def test_lt_diff_numbers_pos_pos(self):
-        pass
-
-    def test_lt_diff_number_neg_neg(self):
-        pass
-
-    def test_lt_diff_numbers_pos_neg(self):
-        pass
-
-    def test_gt_same_numbers_pos(self):
-        pass
-
-    def test_gt_diff_numbers_pos_pos(self):
-        pass
-
-    def test_gt_diff_number_neg_neg(self):
-        pass
-
-    def test_gt_diff_numbers_pos_neg(self):
-        pass
-
 
     def test_pos_short_number_construction(self):
         number = StringNumber("1")
@@ -224,16 +218,4 @@ class StringNumberTests(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    a = StringNumber("1")
-    b = StringNumber("2")
-    c = StringNumber("123")
-    d = StringNumber("74")
-
-    formatable_str = "{0} - {1} = {2}"
-
-    print formatable_str.format(a, b, a - b)
-    print formatable_str.format(b, a, b - a)
-    print formatable_str.format(a, a, a - a)
-    print formatable_str.format(c, d, c - d)
-    print formatable_str.format(d, c, d - c)
-
+    unittest.main()
