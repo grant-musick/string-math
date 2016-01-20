f2068332f2685aebfa7e472b36daa74d04493ed4
grant-musick <grant@cozi.com>
2016-01-19 20:23:12 -0800

    Fixed broken unit tests and refactored problem.

--- python/string_subtraction.py
+++ python/string_subtraction.py
@@ -1,9 +1,32 @@
 #!/usr/bin/python
 # coding: utf-8
 
-import unittest
+
+# The math for these can be broken down into a matrix 
+# based on the negative/postive value of each operand
+# 
+# operator and sign and sign => operation
+# + and +x and +y => add(x,y)
+# + and -x and +y => sub(y,x)
+# + and +x and -y => sub(x,y)
+# + and -x and -y => -add(x,y)
+#
+# - and +x and +y => sub(x,y)
+# - and -x and +y => -add(x,y)
+# - and +x and -y => add(x,y)
+# - and -x and -y => sub(y,x)
+#
+# Then if we are doing subtraction,
+# need to figure out which number is bigger
+# so we can flip again, if necessary
+
+
+
 
 class StringNumber:
+    """Strings that represent numbers (instead of native numeric types)
+    """
+
     def __init__(self, str_number):
         if str_number:
             if str_number.startswith("-"):
@@ -25,20 +48,52 @@ class StringNumber:
             return self.value
 
     def __add__(self, other):
-        return StringNumber.__add(self, other)
+        sval = self.value
+        oval = other.value
+        if not self.negative and not other.negative:
+            return self.__add_process(sval, oval, False)
+        elif self.negative and not other.negative:
+            return self.__sub_process(oval, sval)
+        elif not self.negative and other.negative:
+            return self.__sub_process(sval, oval)
+        else:
+            return self.__add_process(sval, oval, True)
 
     def __sub__(self, other):
-        # This feels like a hack but it makes a test pass
-        if not self.negative and other.negative:
-            return self.__add__(StringNumber(other.value))
-
-        comp = self.__cmp__(other)
-        if comp == 1:
-            return StringNumber.__subtract(self, other, False)
-        elif comp == -1:
-            return StringNumber.__subtract(self, other, True)
+        sval = self.value
+        oval = other.value
+        if not self.negative and not other.negative:
+            return self.__sub_process(sval, oval)
+        elif self.negative and not other.negative:
+            return self.__add_process(sval, oval, True)
+        elif not self.negative and other.negative:
+            return self.__add_process(sval, oval, False)
         else:
-            return StringNumber("0")
+            return self.__sub_process(oval, sval)
+
+    def __add_process(self, val1, val2, flip=False):
+        retval = StringNumber.__add(val1, val2)
+        retval.negative = flip
+        return retval
+
+    def __sub_process(self, val1, val2):
+        comp_val = StringNumber.__compare(val1, val2)
+        sign_flip = False
+        retval = None
+        if comp_val > 0:
+            retval = StringNumber.__subtract(val1, val2)
+            sign_flip = False
+        elif comp_val < 0:
+            retval = StringNumber.__subtract(val2, val1)
+            sign_flip = True
+        else:
+            retval = StringNumber("0")
+        if sign_flip:
+            retval.negative = True
+        return retval
+
+
+
 
     def __cmp__(self, other):
         """Basic cases:
@@ -82,8 +137,8 @@ class StringNumber:
 
     @staticmethod
     def __add(first, second):
-        rev_first = first.value.zfill(len(second))[::-1]
-        rev_second = second.value.zfill(len(first))[::-1]
+        rev_first = first.zfill(len(second))[::-1]
+        rev_second = second.zfill(len(first))[::-1]
 
         answer_coll = []
         add_one_to_next = False;
@@ -114,13 +169,13 @@ class StringNumber:
         return StringNumber(str(answer_str))
 
     @staticmethod
-    def __subtract(minuend, subtrahend, sign_flip=False):
+    def __subtract(minuend, subtrahend):
         """ Assuming we already know the minuend is a numeric value
         greater than the subtrahend
         """
         # Using Austrian method
-        rev_minu = minuend.value[::-1]
-        rev_subt = subtrahend.value.zfill(len(rev_minu))[::-1]
+        rev_minu = minuend[::-1]
+        rev_subt = subtrahend.zfill(len(rev_minu))[::-1]
 
         answer_coll = []
         add_one_to_next = False
@@ -141,11 +196,16 @@ class StringNumber:
         answer_str = "".join(answer_arr)
         answer_str = answer_str[::-1].lstrip('0')
         if not answer_str: answer_str = '0'
-        if sign_flip:
-            answer_str = "-" + answer_str
         return StringNumber(answer_str)
 
+
+
+
+
 if __name__ == "__main__":
+    """Canary in the coal mine versions just to make things even sort of work
+    before going into test framework
+    """
     a = StringNumber("1")
     b = StringNumber("2")
     c = StringNumber("123")
